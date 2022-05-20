c93be7c7bdd06c03dd3dca6c7ad4a5e2c07ec67b
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2022-05-20 07:14:51 +0700

    webmanifest correect error

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
@@ -39,6 +40,29 @@ module.exports = {
     }),
     new ServiceWorkerWebpackPlugin({
       entry: path.resolve(__dirname, 'src/scripts/sw.js'),
+    }),
+    new WebpackPwaManifest({
+      name: 'Restopedia',
+      short_name: 'Restoped',
+      description:
+        'Restopedia is a simple app that helps you find restaurants in your area',
+      background_color: '#000000',
+      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve('src/assets/favicon.png'),
+          sizes: [96, 128, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve('src/assets/favicon.png'),
+          size: '1024x1024', // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('src/assets/favicon.png'),
+          size: '1024x1024',
+          purpose: 'maskable',
+        },
+      ],
     }),
   ],
 };
